e35adff3d5a337bea98124676c8b84644fe0e20b
techLinky <gcebile.sne@gmail.com>
2023-09-13 19:52:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.If_Staments_Using_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Enter number 1");
+            int num1 = Console.Read();
+            Console.WriteLine("Enter number 2");
+            int num2 = Console.Read();
+
+            Console.WriteLine(GetMax(num1,num2));
+
+            //freeze console
+            Console.ReadLine();
+
+        }
+        static int GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+               Console.WriteLine("The max is ");
+                return num1;
+            }
+            else
+            {
+                Console.WriteLine("The max is ");
+                return num2;
+            }
+
+        }
+        
+            
+    }
+
+}
